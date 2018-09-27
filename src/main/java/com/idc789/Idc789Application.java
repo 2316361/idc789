dd775b47b56209ad3601589e8fb997dfa3783d28
iam2316361 <air2316361@sina.com>
2018-09-27 10:31:31 +0800

    1

--- src/main/java/com/idc789/Idc789Application.java
+++ src/main/java/com/idc789/Idc789Application.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
-//@EnableScheduling
+@EnableScheduling
 public class Idc789Application {
 	public static void main(String[] args) {
 		SpringApplication.run(Idc789Application.class, args);
